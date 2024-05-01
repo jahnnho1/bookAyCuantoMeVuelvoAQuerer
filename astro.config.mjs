45cb2cfcd2b7ef613a8053c6f42c7dc83d3d8d4e
Clymo <jahnnho.moraga@gmail.com>
2024-05-01 15:07:30 -0400

    se agrego netlify data

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,26 +4,25 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
+import netlify from "@astrojs/netlify";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://www.github.com/jahnnho',
   integrations: [mdx(), sitemap(), tailwind()],
   markdown: {
-    remarkPlugins: [
-      remarkMath,
-    ],
-    rehypePlugins: [
-      () => async (tree) => {
-        for (const node of tree.children) {
-          if (node.tagName === 'pre' && node.properties && node.properties.className) {
-            node.properties.className.push('mockup-code');
-          }
-          if (node.tagName === 'blockquote' && node.properties) {
-			node.properties.className = ['alert'];
-          }
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [() => async tree => {
+      for (const node of tree.children) {
+        if (node.tagName === 'pre' && node.properties && node.properties.className) {
+          node.properties.className.push('mockup-code');
         }
-      },
-      rehypeKatex
-    ]
-  }
+        if (node.tagName === 'blockquote' && node.properties) {
+          node.properties.className = ['alert'];
+        }
+      }
+    }, rehypeKatex]
+  },
+  output: "server",
+  adapter: netlify()
 });
